4254a7247c2710820ca1a12f134d2cac803fca58
Genji <genji-fujimori@cybozu.co.jp>
2023-09-11 23:34:35 +0900

    create custom cover

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -1,5 +1,36 @@
+<!--
+Usage:
+
+```md
+---
+layout: clover
+image: URL or path to image in public folder
+---
+
+# Title
+
+Text
+```
+-->
+<script setup lang="ts">
+import { computed } from 'vue'
+import { handleBackground } from '../utils'
+
+const props = defineProps({
+  image: {
+    // random image from a curated Unsplash collection by Anthony
+    default: 'https://source.unsplash.com/collection/94734566/1920x1080',
+  },
+})
+
+const style = computed(() => handleBackground(props.image, true))
+</script>
+
 <template>
-  <div class="slidev-layout cover">
+  <div
+    class="slidev-layout cover text-center"
+    :style="style"
+  >
     <div class="my-auto w-full">
       <slot />
     </div>
