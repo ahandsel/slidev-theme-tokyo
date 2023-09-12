923f8b0b4e15623abe074f4ebc4657d69f3dfbe0
Genji <genji-fujimori@cybozu.co.jp>
2023-09-12 22:22:36 +0900

    Update cover-light.vue

--- layouts/cover-light.vue
+++ layouts/cover-light.vue
@@ -22,16 +22,13 @@ const props = defineProps({
     // random image from a curated Unsplash collection by Anthony
     default: 'https://source.unsplash.com/collection/94734566/1920x1080',
   },
-})
+});
 
 const style = computed(() => handleBackground(props.image, false))
 </script>
 
 <template>
-  <div
-    class="slidev-layout cover text-center"
-    :style="style"
-  >
+  <div class="slidev-layout cover text-center" :style="style">
     <div class="my-auto w-full">
       <slot />
     </div>
