0850b005195e0fb7399ffed08b5835560acc8377
Genji <genji-fujimori@cybozu.co.jp>
2023-09-12 22:22:28 +0900

    Update intro.vue

--- layouts/intro.vue
+++ layouts/intro.vue
@@ -1,3 +1,17 @@
+<!--
+Usage:
+
+```md
+---
+layout: intro
+image: URL or path to image in public folder
+---
+
+# Title
+
+Text
+```
+-->
 <template>
   <div class="slidev-layout intro">
     <div class="my-auto">
